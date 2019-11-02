cc15bd8bfa8e6d5d37c949ac4f3789c441514a69
Wagner-Becker <wagner__becker@hotmail.com>
2019-11-02 14:57:44 -0300

    Menu atualizado

--- Trab.c
+++ Trab.c
@@ -1,59 +1,66 @@
 #include <stdio.h>
 int main()
 {
-	int menu;
-	func_print();
-	if(menu=1)
+	int menu=0;
+	while(menu!=6)
 	{
-		func_cadastrar();
+		
+		func_print();
+		scanf("%d",&menu);
+		if(menu==1)
+		{
+			func_cadastrar();
+		}
+		if(menu==2)
+		{
+			func_listar();
+		}
+		if(menu==3)
+		{
+			func_pesquisar();
+		}
+		if(menu==4)
+		{
+			func_atualizar();
+		}
+		if(menu==5)
+		{
+			func_remover();
+		}	
 	}
-	if(menu=2)
-	{
-		func_listar();
-	}
-	if(menu=3)
-	{
-		func_pesquisar();
-	}
-	if(menu=4)
-	{
-		func_atualizar();
-	}
-	if(menu=5)
-	{
-		func_remover();
-	}	
-
+	printf("\t\tVoce escolheu SAIR");
 	return 0;
 }
 int func_print()
 {
-	printf("Cadastro de pessoas\n");
-	printf("1 - Cadastrar\n");
-	printf("2 - Listar\n");
-	printf("3 - Pesquisar\n");
-	printf("4 - Atualizar\n");
-	printf("5 - Remover\n");
-	printf("6 - Sair\n");
+	printf("\t\t----------------------\n");
+	printf("\t\t| Cadastro de pessoas|\n");
+	printf("\t\t| 1 - Cadastrar      |\n");
+	printf("\t\t| 2 - Listar         |\n");
+	printf("\t\t| 3 - Pesquisar      |\n");
+	printf("\t\t| 4 - Atualizar      |\n");
+	printf("\t\t| 5 - Remover        |\n");
+	printf("\t\t| 6 - Sair           |\n");
+	printf("\t\t----------------------\n");
 
 }
 int func_cadastrar()
 {
-
+	printf("\t\tVoce escolheu CADASTRAR\n");
 }
 int func_listar()
 {
-
+	printf("\t\tVoce escolheu LISTAR\n");
 }
 int func_pesquisar()
 {
-
+	printf("\t\tVoce escolheu PESQUISAR\n");
 }
 int func_atualizar()
 {
-
+	printf("\t\tVoce escolheu ATUALIZAR\n");
 }
 int func_remover()
 {
-
+	printf("\t\tVoce escolheu REMOVER\n");
 }
